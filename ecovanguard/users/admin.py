f8613105781f5f054d4abbea40461c0ace03edce
Ibrahim <ibraheemabdulmojeed1@gmail.com>
2024-02-28 21:31:23 +0100

    Delete ecovanguard directory

--- ecovanguard/users/admin.py
+++
@@ -1,8 +0,0 @@
-# from django.contrib import admin
-
-# from .models import User, UserProfile, ExecutivePosition, ExecutiveProfile
-
-# admin.site.register(User)
-# admin.site.register(UserProfile)
-# admin.site.register(ExecutivePosition)
-# admin.site.register(ExecutiveProfile)
