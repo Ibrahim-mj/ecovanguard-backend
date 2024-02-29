896124890ca924c3ad5cc6c6b61fb860e1966de7
Ibrahim <ibraheemabdulmojeed1@gmail.com>
2024-02-29 09:00:46 +0100

    modify the custom storage backend to ignore missing source files

--- ecovanguard/storage_backends.py
+++ ecovanguard/storage_backends.py
@@ -1,9 +1,11 @@
 from whitenoise.storage import CompressedManifestStaticFilesStorage
+from whitenoise.storage import MissingFileError
 
 class CustomStaticFilesStorage(CompressedManifestStaticFilesStorage):
     def post_process(self, paths, dry_run=False, **options):
-        # ignore errors
-        try:
-            yield from super().post_process(paths, dry_run, **options)
-        except Exception as e:
-            print(str(e))
+        # ignore missing source map files
+        for name, hashed_name, processed in super().post_process(paths, dry_run, **options):
+            if isinstance(processed, MissingFileError):
+                print(f"Skipping file {name} due to MissingFileError")
+                continue
+            yield name, hashed_name, processed
