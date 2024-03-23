7fb7c0f8cd85b2e1218e11451ee48a5f24052a2f
Ibrahim <ibraheemabdulmojeed1@gmail.com>
2024-03-23 13:15:42 +0100

    Fixed whitenoise configuration

--- ecovanguard/settings.py
+++ ecovanguard/settings.py
@@ -133,10 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") # For Render deployment
-
-    STATICFILES_STORAGE = 'ecovanguard.storage_backends.CustomStaticFilesStorage' # Custom storage backend that inherits from CompressedManifestStaticFilesStorage. This is to ignore postprocess errors when running collectstatic
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
