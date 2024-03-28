1f186257a7e9c7ca9e69d595c6671e64a96ff20b
Ibrahim <ibraheemabdulmojeed1@gmail.com>
2024-03-28 11:54:02 +0100

    fixed db set up

--- ecovanguard/settings.py
+++ ecovanguard/settings.py
@@ -90,16 +90,24 @@ WSGI_APPLICATION = "ecovanguard.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": config("NAME"),
+#         "USER": config("USER"),
+#         "PASSWORD": config("PASSWORD"),
+#         "HOST": config("HOST"),
+#         # "PORT": config("PORT")
+#     }
+# }
+
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("NAME"),
-        "USER": config("USER"),
-        "PASSWORD": config("PASSWORD"),
-        "HOST": config("HOST"),
-        # "PORT": config("PORT")
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
+
+DATABASES['default']['NAME'] = 'eco_dev'
 
 
 # Password validation
